3202a6c91b689f7ef501baee0215c0e07e00a1e5
Amila <155749022+Savinda1@users.noreply.github.com>
2025-09-22 00:46:06 +0530

    asynchronous state (like RTK Query)

--- src/lib/api/api.js
+++ src/lib/api/api.js
@@ -7,6 +7,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
+
 export const formApi = createApi({
   reducerPath: "formApi",
   baseQuery: fetchBaseQuery({ baseUrl: `${BACKEND_URL}/api/` }), 
